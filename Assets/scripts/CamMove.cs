64bf898a2f81c780fc79e3c785f600f9b62861ff
AbhishekRaj1201 <abhishekraj111001@gmail.com>
2023-07-16 01:08:58 +0530

    Dancing-Ninja

---
+++ Assets/scripts/CamMove.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamMove : MonoBehaviour
+{
+    public float H = 2.0f;
+    public float V = 2.0f;
+
+    private float a = 0.0f;
+    private float b = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a += H * Input.GetAxis("Mouse X");
+        b -= V * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(a, b, 0.0f);
+    }
+}
---
+++ Assets/scripts/PlayerScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Animator anim;
+    public bool isone=false;
+    public bool istwo=false;
+    public bool isthree=false;
+    public bool isfour=false;
+    public bool isfive=false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isone = Input.GetKey(KeyCode.UpArrow);
+        anim.SetBool("one", isone);
+
+        istwo = Input.GetKey(KeyCode.DownArrow);
+        anim.SetBool("two", istwo);
+
+        isthree = Input.GetKey(KeyCode.LeftArrow);
+        anim.SetBool("three", isthree);
+
+        isfour = Input.GetKey(KeyCode.RightArrow);
+        anim.SetBool("four", isfour);
+
+        isfive = Input.GetKey(KeyCode.RightShift);
+        anim.SetBool("five", isfive);
+
+
+    }
+}
